11a341db8378a0c9770f21db2a7b5357fe059f21
Juan De Dios Santos <me@juandes.com>
2021-07-10 21:16:48 +0200

    Added quant model

--- convert/tensorflow-lite/main.py
+++ convert/tensorflow-lite/main.py
@@ -6,5 +6,22 @@ converter = tf.lite.TFLiteConverter.from_saved_model(
 tflite_model = converter.convert()
 
 # Save the model.
-with open('../../models/steps-regression/tflite/model.tflite', 'wb') as f:
+with open('../../models/steps-regression/tflite/modellll.tflite', 'wb') as f:
     f.write(tflite_model)
+
+
+# With quantization
+# Floating point -> integer
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_quant_model = converter.convert()
+
+with open('../../models/steps-regression/tflite/model_int_quant.tflite', 'wb') as f:
+    f.write(tflite_quant_model)
+
+
+# Float16 quant
+converter.target_spec.supported_types = [tf.float16]
+tflite_quant_model = converter.convert()
+
+with open('../../models/steps-regression/tflite/model_float16_quant.tflite', 'wb') as f:
+    f.write(tflite_quant_model)
